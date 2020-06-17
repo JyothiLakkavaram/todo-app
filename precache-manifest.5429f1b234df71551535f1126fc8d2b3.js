c5f77adb0d60b20399481dab8fe81c8abfa5509a
Jyothi <jyothi.lakkavaram@gmail.com>
2020-06-17 21:58:09 +0530

    todo

---
+++ precache-manifest.5429f1b234df71551535f1126fc8d2b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "367c43254dd50233b9fd331a75c67677",
+    "url": "/index.html"
+  },
+  {
+    "revision": "334cdc0a912d2c616dc5",
+    "url": "/static/css/main.8847664a.chunk.css"
+  },
+  {
+    "revision": "a39afc45bbf798f9a451",
+    "url": "/static/js/2.cf564c9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cf564c9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "334cdc0a912d2c616dc5",
+    "url": "/static/js/main.bbe8c0ab.chunk.js"
+  },
+  {
+    "revision": "8b4b5af2ab4f67603a00",
+    "url": "/static/js/runtime-main.b0654ce4.js"
+  }
+]);
